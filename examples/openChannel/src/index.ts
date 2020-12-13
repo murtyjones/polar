61bc5ad73189e89b599b25bd788a4421997d9a39
Marty Jones <murtin.jones@gmail.com>
2020-12-13 10:18:34 -0500

    wip

--- examples/openChannel/src/index.ts
+++ examples/openChannel/src/index.ts
@@ -1,11 +1,24 @@
 import ln from 'ln-service';
 import { ALICE_LND_AUTH } from './config';
 
+const openChannel = async (lnd: ln.AuthenticatedLND) => {
+  const { peers } = await ln.getPeers({ lnd });
+  const args: ln.OpenChannelArgs = {
+    local_tokens: 100000,
+    partner_public_key: peers[0].public_key,
+  };
+  return ln.openChannel({ lnd, ...args });
+};
+
 (async () => {
   const { lnd } = ln.authenticatedLndGrpc(ALICE_LND_AUTH);
   try {
-    const height = await ln.getHeight({ lnd });
-    console.log(height);
+    const { pending_channels } = await ln.getPendingChannels({ lnd });
+    if (!pending_channels.length) {
+      const { transaction_id } = await openChannel(lnd);
+      console.log('No pending channels found, opened one: ', transaction_id);
+    }
+    console.log('Pending channels found: ', pending_channels.length);
   } catch (err) {
     console.error(err);
   }
